2d69e1b4d1b8393ac7092325211858f68ba2f787
Marcos <marcoslucben@gmail.com>
2024-02-14 01:05:21 -0300

    Inicializar Springboot

---
+++ backend/yow/src/main/java/com/no_country/yow/YowApplication.java
@@ -0,0 +1,13 @@
+package com.no_country.yow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YowApplication.class, args);
+	}
+
+}
---
+++ backend/yow/src/test/java/com/no_country/yow/YowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no_country.yow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
