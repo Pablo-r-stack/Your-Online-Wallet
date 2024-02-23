f465eb4678d0dcf25edd8524d4695f015022d0e5
Jose Ramirez <jm_rg@comunidad.unam.mx>
2024-02-22 23:01:12 -0600

    properties update

--- backend/yow/src/main/java/com/no_country/yow/controllers/CountryController.java
+++ backend/yow/src/main/java/com/no_country/yow/controllers/CountryController.java
@@ -3,14 +3,12 @@ package com.no_country.yow.controllers;
 import com.no_country.yow.models.Countries;
 import com.no_country.yow.services.IServiceCountry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
