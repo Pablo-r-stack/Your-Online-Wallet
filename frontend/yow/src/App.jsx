40c9f0dff3b61e9f12df52bcd8b74c4f31dfb1e5
Patricia <patchavez165@gmail.com>
2024-02-23 19:43:04 -0300

    Se agrego icono a componente navbar

--- frontend/yow/src/App.jsx
+++ frontend/yow/src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Footer from './components/Footer'
 import HomePage from './pages/HomePage'
-
+import Navbar1 from './components/Navbar1'
 
 function App() {
 
   return (
     <BrowserRouter>
+     <Navbar1/>
       <Routes>
         <Route path='/' element={<HomePage />}></Route>
       </Routes>
--- frontend/yow/src/components/Navbar1.jsx
+++ frontend/yow/src/components/Navbar1.jsx
@@ -1,18 +1,21 @@
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Navbar1 = () => {
   return (
-    <nav className="bg-gray-800 text-white py-4">
+    <nav className="bg-zinc-900 text-white py-4">
       <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
         <div className="flex items-center">
-          <span className="text-lg font-bold">Mi Sitio Web</span>
+        <img className='w-16 mr-3 rounded-full' src="\src\img\icons\logo.png" alt="wallet icon" />
+          <span className="text-lg font-bold">You Online Wallet</span>
+
         </div>
         <div className="flex space-x-4">
-          <Link to="/pagina1" className="text-white hover:text-gray-300">Pagina 1</Link>
-          <Link to="/pagina2" className="text-white hover:text-gray-300">Pagina 2</Link>
-          <Link to="/pagina3" className="text-white hover:text-gray-300">Pagina 3</Link>
+          <Link to="/" className="text-white hover:text-gray-300">Home</Link>
+          <Link to="/register" className="text-white hover:text-gray-300">Registro</Link>
+          <Link to="/login" className="text-white hover:text-gray-300">Login</Link>
         </div>
       </div>
     </nav>
