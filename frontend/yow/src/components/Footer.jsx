a4aff5c913a5be66f9919b5ddad3235740f77266
Pablo-r-stack <pablor.velasco.suarez@gmail.com>
2024-02-23 18:56:34 -0300

    se añadio vista HomePage, componente Footer y Button, se agrego ruta a App.jsx

---
+++ frontend/yow/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Footer() {
+    return (
+        <footer className='container; text-center text-sm space-y-0 font-sans'>
+            <ul className='inline-flex space-x-1 font-normal text-blue-900'>
+                <li className='hover:font-medium transition-all'><Link>Home</Link></li>
+                <li className='hover:font-medium transition-all'><Link>Contact</Link></li>
+                <li className='hover:font-medium transition-all'><Link>Faq</Link></li>
+            </ul>
+            <h6 className='font-semibold text-gray-800'>2024- Your Online Wallet ®</h6>
+            <p className='text-xs italic'>all rights reserved</p>
+        </footer>
+    )
+}
---
+++ frontend/yow/src/pages/HomePage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Button from '../components/btn/Button'
+import { useNavigate } from 'react-router-dom';
+export default function HomePage() {
+    const navigate = useNavigate();
+
+    const handleRedirect = (path) => {
+      navigate(path);
+    };
+
+    return (
+        <body className='container max-w-full h-screen font-sans'>
+          <section className='bg-zinc-900 h-2/4 p-8 lg:p-20 flex flex-col lg:flex-row'>
+            <div className='text-slate-200 w-full lg:w-2/4 mt-8 lg:mt-0 ml-0 lg:ml-8'>
+              <h1 className='text-3xl lg:text-7xl font-bold'>Bienvenido a tu nueva Economía</h1>
+              <p className='text-lg lg:text-2xl my-4'>Vive tu Experiencia Yow.</p>
+            </div>
+            <div className='w-full lg:w-2/4 flex justify-center items-center lg:justify-end'>
+              <img className='w-2/3 lg:w-1/3 lg:m-72 rounded-full' src="\src\img\icons\logo.png" alt="wallet icon" />
+            </div>
+          </section>
+          <section className='h-2/4 text-zinc-900  text-center flex flex-col justify-center'>
+            <h2 className='text-3xl lg:text-5xl font-medium my-4 lg:my-10'>Bienvenido a Your Online Wallet</h2>
+            <div className='space-y-4 lg:space-x-10 lg:space-y-0 flex flex-col lg:flex-row items-center justify-center'>
+              <Button text="Registrar" onClick={() => handleRedirect('/')} />
+              <Button text="Login" onClick={() => handleRedirect('/')} />
+              <Button text="Acerca de" onClick={() => handleRedirect('/')} />
+            </div>
+          </section>
+        </body>
+      );
+    }
