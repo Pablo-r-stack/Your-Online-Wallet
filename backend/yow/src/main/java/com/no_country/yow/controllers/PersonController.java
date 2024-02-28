905afd4922ba358956f3053d5904cf52f45276c9
Jose Ramirez <jm_rg@comunidad.unam.mx>
2024-02-28 14:20:21 -0600

    Controladores de Country y Person

--- backend/yow/src/main/java/com/no_country/yow/controllers/PersonController.java
+++ backend/yow/src/main/java/com/no_country/yow/controllers/PersonController.java
@@ -1,6 +1,9 @@
 package com.no_country.yow.controllers;
 
+import com.no_country.yow.exceptions.YOWException;
 import com.no_country.yow.models.Person;
+import com.no_country.yow.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,11 +11,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class PersonController {
 
-    @PutMapping("/modifiedUser")
-    public ResponseEntity<?> updatePassword(@RequestBody Person person){
+    @Autowired
+    private PersonService service;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findUserById(@PathVariable Long id) throws YOWException {
+        return service.findById(id);
+    }
 
-        return ResponseEntity.ok("");
-
+    @PutMapping("/modifiedUser/{id}")
+    public ResponseEntity<?> updatePassword(@RequestBody Person person, @PathVariable Long id) throws YOWException {
+        return service.updateById(person, id);
     }
 }
