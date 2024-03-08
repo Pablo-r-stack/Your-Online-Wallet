22af6cab4257cfed05c423c55ac5ee4c801c0ba2
Pablo-r-stack <pablor.velasco.suarez@gmail.com>
2024-03-08 02:21:44 -0300

    funcionalidad y conexion con backend modal de transferencia

--- frontend/yow/src/auth/WalletProvider.jsx
+++ frontend/yow/src/auth/WalletProvider.jsx
@@ -49,8 +49,26 @@ export const WalletProvider = ({ children }) => {
     }
   }
 
+  const mountTransfer = async (numberDocument, userReciever) => {
+    try {
+      const token = localStorage.getItem('token');
+      const config = {
+        headers: { Authorization: `Bearer ${token}` }
+      };
+      console.log(`Data in transfer ${JSON.stringify(userReciever)}`);
+      console.log('Requesting authorization to server');
+      console.log('Authorization granted');
+      const response = await axios.post(`${API_URL}/initiated/transfer/${numberDocument}`, userReciever, config);
+      console.log('Server response succeed');
+      console.log('Transfer was successful');
+      getAccount();
+    } catch (error) {
+      console.log('Something went wrong');
+    }
+  }
+
   return (
-    <WalletContext.Provider value={{ getAccount, getWallet, wallet, addFunds }}>
+    <WalletContext.Provider value={{ getAccount, getWallet, wallet, addFunds, mountTransfer }}>
       {children}
     </WalletContext.Provider>
   );
